ca4c22b764746c56818f1605485023845fd74f09
Sylwia Waligora <sylwiavv@gmail.com>
2019-06-26 20:16:56 +0200

    Add next task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends Component {
+    state = {
+        tasks: this.props.tasks,
+        draft: ''
+    };
+
+    upDateDraft = event => {
+        this.setState({draft: event.target.value})
+    };
+
+    addToDo = () => {
+        const { tasks, draft } = this.state;
+        const list = tasks;
+        list.push(draft);
+        this.setState({tasks: list, draft: ''})
+    };
+
+    render() {
+        const { title } = this.props;
+        const { tasks, draft } = this.state;
+
+        return (
+            <div className="app-container">
+                <h3>{this.props.title}</h3>
+                <input type='text' onChange={this.upDateDraft} value={draft} />
+                <button onClick={this.addToDo} className="btn">Dodaj zadanie</button>
+                <ul className="tasks-container">
+                    {tasks.map((task,i) => <li key={'task ' + i} className="single-task">{task} <i className="far fa-trash-alt"></i></li> )}
+                </ul>
+            </div>
+        );
+    }
 }
+
+class App extends Component {
+    myTasks = [
+        'Zadanie 1',
+        'Zadnie 2',
+        'Zadanie 3',
+        'Zadanie 4'
+    ];
+
+  render() {
+    return (
+        <div>
+          <ToDoList title='Moja lista zadań' tasks={this.myTasks}  />
+        </div>
+    );
+  }
+}
+
 
 export default App;
